b6129f624593aa444a1422d38f0d3fc321529c32
cp3 <792716300@qq.com>
2022-04-04 18:40:38 +0800

    init

---
+++ src/main/java/org/cp3geek/onlinejudge_backend/OnlinejudgeBackendApplication.java
@@ -0,0 +1,13 @@
+package org.cp3geek.onlinejudge_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinejudgeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinejudgeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cp3geek/onlinejudge_backend/OnlinejudgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cp3geek.onlinejudge_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinejudgeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
