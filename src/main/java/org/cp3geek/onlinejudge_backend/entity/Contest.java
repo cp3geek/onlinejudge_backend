aa28520fc632ec075274932eec797fad39a45844
cp3 <792716300@qq.com>
2022-04-13 15:12:49 +0800

    feature(entity): 设计数据库表

--- src/main/java/org/cp3geek/onlinejudge_backend/entity/Contest.java
+++ src/main/java/org/cp3geek/onlinejudge_backend/entity/Contest.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 @Data
@@ -19,5 +20,14 @@ public class Contest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long contestId;
+    private String contestName;//比赛名称
+    private String contestType;//比赛类型
+    private String contestDescription;//比赛描述
+    private String contestState;//比赛状态
+    private Date contestStartTime;//比赛开始时间
+    private Date contestEndTime;//比赛结束时间
+    private String contestHost;//举办者
+    private int problemNumber;//题目数量
+
 
 }
--- src/main/java/org/cp3geek/onlinejudge_backend/entity/Problem.java
+++ src/main/java/org/cp3geek/onlinejudge_backend/entity/Problem.java
@@ -21,11 +21,11 @@ public class Problem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long problemId;
-    private String problemTitle;
-    private String problemDescription;
-    private String input;
-    private String output;
-    private String samples;
-    private Date updateTime;
-    private Date createTime;
+    private String problemTitle;//题目标题
+    private String problemDescription;//题目描述
+    private String input;//输入样例
+    private String output;//输出样例
+    private String samples;//输入输出测试样例
+    private Date updateTime;//题目更新时间
+    private Date createTime;//题目创建时间
 }
