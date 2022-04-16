f1a939aa58b5575b9d873ef0eb76869e340ddd20
cp3 <792716300@qq.com>
2022-04-16 18:32:37 +0800

    test(controller): 测试SpringSecurity

--- src/main/java/org/cp3geek/onlinejudge_backend/controller/UserController.java
+++ src/main/java/org/cp3geek/onlinejudge_backend/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,6 +18,10 @@ public class UserController {
        User user = userService.userLogin(userName,userPassword);
         System.out.println(user.getUserName());
         return user;
+    }
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
     }
 
 }
